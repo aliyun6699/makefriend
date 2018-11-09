c736e4a0c9a35f72583e31c6ae5bd25cfbb1e008
fangbo <hh@qq.com>
2018-11-09 06:04:30 -0800

    kkkkkkkkkk

---
+++ src/main/java/com/atguigu/maven/MakeFriends.java
@@ -0,0 +1,12 @@
+package com.atguigu.maven;
+
+public class MakeFriends {
+	public String makeFriends(String name){
+		HelloFriend friend = new HelloFriend();
+		friend.sayHelloToFriend("litingwei");
+		String str = "Hey,"+friend.getMyName()+" make a friend please.";
+		System.out.println(str);
+		return str;
+	}
+
+}
